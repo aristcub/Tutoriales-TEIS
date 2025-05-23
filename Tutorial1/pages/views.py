428a62da0541583c31bace624d812eb7a4ce0f0c
Dix <admin@Dixons-MacBook-Pro.local>
2025-05-22 22:57:02 -0500

    Tutorial 1, actividad 3

--- Tutorial1/pages/views.py
+++ Tutorial1/pages/views.py
@@ -37,11 +37,12 @@ class ContactPageView(TemplateView):
 
 class Product:
     products = [
-        {"id": "1", "name": "TV", "description": "Best TV"},
-        {"id": "2", "name": "iPhone", "description": "Best iPhone"},
-        {"id": "3", "name": "Chromecast", "description": "Best Chromecast"},
-        {"id": "4", "name": "Glasses", "description": "Best Glasses"},
+        {"id": "1", "name": "TV", "description": "Best TV", "price": 1500},
+        {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 999},
+        {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 49},
+        {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 199},
     ]
+
 
 
 class ProductIndexView(View):
