50170a599583958d1e348c8617101275a90e4138
Dix <admin@Dixons-MacBook-Pro.local>
2025-05-22 23:21:03 -0500

    Tutorial 1, actividad 4,5,6,7,8 y 9

--- Tutorial1/pages/views.py
+++ Tutorial1/pages/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import TemplateView
 from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django import forms
+
+
 
 # ------------------ Static Pages ------------------
 
@@ -41,6 +46,7 @@ class Product:
         {"id": "2", "name": "iPhone", "description": "Best iPhone", "price": 999},
         {"id": "3", "name": "Chromecast", "description": "Best Chromecast", "price": 49},
         {"id": "4", "name": "Glasses", "description": "Best Glasses", "price": 199},
+        {"id": "5", "name": "Airpods", "description": "Pruebita", "price": 2},
     ]
 
 
@@ -61,10 +67,68 @@ class ProductShowView(View):
     template_name = 'pages/products/show.html'
 
     def get(self, request, id):
-        product = Product.products[int(id) - 1]
+        try:
+            product = Product.products[int(id) - 1]
+        except (IndexError, ValueError):
+            return HttpResponseRedirect(reverse('home'))  # Redirige si el ID es inválido
+
         viewData = {
             "title": f"{product['name']} - Online Store",
             "subtitle": f"{product['name']} - Product information",
             "product": product
         }
         return render(request, self.template_name, viewData)
+
+# ------------------ Product Creation Form ------------------
+
+class ProductForm(forms.Form):
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+
+    def clean_price(self):
+        price = self.cleaned_data['price']
+        if price <= 0:
+            raise forms.ValidationError("Price must be greater than zero.")
+        return price
+
+class ProductCreateView(View):
+    template_name = 'pages/products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {
+            "title": "Create product",
+            "form": form
+        }
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            new_product = {
+                "id": str(len(Product.products) + 1),
+                "name": form.cleaned_data["name"],
+                "description": "No description",
+                "price": form.cleaned_data["price"]
+            }
+            Product.products.append(new_product)
+
+            return render(request, 'pages/products/success.html')  # 👈 cambia esto
+        else:
+            viewData = {
+                "title": "Create product",
+                "form": form
+            }
+            return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            # Aquí podrías guardar el producto si estuvieras usando modelos reales
+            return redirect('products.index')  # Redirige a la lista de productos
+        else:
+            viewData = {
+                "title": "Create product",
+                "form": form
+            }
+            return render(request, self.template_name, viewData)
