3f6015fcb231e428afdd8af6bdd894921bc81356
Dix <admin@Dixons-MacBook-Pro.local>
2025-05-22 22:29:22 -0500

    tutorial 1, actividad 1

--- Tutorial1/pages/views.py
+++ Tutorial1/pages/views.py
@@ -1,6 +1,31 @@
 from django.shortcuts import render
-from django.http import HttpResponse # new
+from django.views.generic import TemplateView
 
-def homePageView(request): # new
- return HttpResponse('Hello World!') # new
-# Create your views here.
+class HomePageView(TemplateView):
+    template_name = 'pages/home.html'
+
+
+class AboutPageView(TemplateView): 
+    template_name = 'pages/about.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs) 
+        context.update({
+            "title": "About us - Online Store", 
+            "subtitle": "About us",
+            "description": "This is an about page ...", 
+            "author": "Developed by: Your Name",
+        })
+        return context
+
+
+class ContactPageView(TemplateView):
+    template_name = 'pages/contact.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context.update({
+            "title": "Contact - Online Store",
+            "subtitle": "Contact Us",
+        })
+        return context
