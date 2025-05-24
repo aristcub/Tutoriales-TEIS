77556ecf0fcf84c8d2b8c43253ca4b5c73a401b4
Dix <admin@Dixons-MacBook-Pro.local>
2025-05-23 19:39:12 -0500

    Avance Api

--- todoapp/api/urls.py
+++ todoapp/api/urls.py
@@ -3,4 +3,8 @@ from . import views
 
 urlpatterns = [
     path('todos/', views.ToDoListCreate.as_view(), name='list'),
+    path('todos/<int:pk>', views.TodoRetrieveUpdateDestroy.as_view(), name='detail'),
+    path('todos/<int:pk>/complete', views.TodoToggleComplete.as_view(), name='complete'),
+    path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
 ]
--- todoapp/api/views.py
+++ todoapp/api/views.py
@@ -1,16 +1,83 @@
-from rest_framework import generics
-from .serializers import ToDoSerializer
+from rest_framework import generics, permissions
+from .serializers import ToDoSerializer, TodoToggleCompleteSerializer
 from todo.models import ToDo
-
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token 
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt 
+from django.contrib.auth import authenticate
 
 class ToDoListCreate(generics.ListCreateAPIView):
-    # ListCreateAPIView requires two mandatory attributes: serializer_class and get_queryset.
     serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         user = self.request.user
         return ToDo.objects.filter(user=user).order_by('-created')
 
     def perform_create(self, serializer):
-        # serializer holds a Django model
         serializer.save(user=self.request.user)
+
+
+class TodoRetrieveUpdateDestroy(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ToDoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        # user can only update, delete own posts
+        return ToDo.objects.filter(user=user)
+    
+
+class TodoToggleComplete(generics.UpdateAPIView):
+    serializer_class = TodoToggleCompleteSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        return ToDo.objects.filter(user=user)
+
+    def perform_update(self, serializer):
+        serializer.instance.completed = not serializer.instance.completed
+        serializer.save()
+
+@csrf_exempt
+def signup(request):
+    if request.method == 'POST':
+        try:
+            data = JSONParser().parse(request)  # data is a dictionary
+            user = User.objects.create_user(
+                username=data['username'],
+                password=data['password']
+            )
+            user.save()
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error': 'Username taken. Choose another username.'},
+                status=400
+            )
+
+@csrf_exempt
+def login(request):
+    if request.method == 'POST':
+        data = JSONParser().parse(request)
+        user = authenticate(
+            request,
+            username=data['username'],
+            password=data['password']
+        )
+        if user is None:
+            return JsonResponse(
+                {'error': 'Unable to login. Check username and password.'},
+                status=400
+            )
+        else:
+            try:
+                token = Token.objects.get(user=user)
+            except Token.DoesNotExist:
+                token = Token.objects.create(user=user)
+            return JsonResponse({'token': str(token)}, status=201)
